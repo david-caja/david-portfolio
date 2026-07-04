abfabfca5095c5af14ab26051330a91b281c7f7f
david-caja <cajafreest@gmail.com>
2026-07-04 18:14:26 +0000

    modification du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <footer className="py-10 bg-gray-900 text-white text-center">
-      <p>&copy; {new Date().getFullYear()} Mon Portfolio. Tous droits réservés.</p>
-      <p className="text-sm text-gray-400 mt-2">Développé avec React et Tailwind CSS</p>
+      <p>&copy; {new Date().getFullYear()} Dzahini David Winfried. Tous droits réservés.</p>
+      <p className="text-sm text-gray-400 mt-2">Toujours ouvert aux nouvelles opportunités, collaborations et défis innovants. Travaillons ensemble pour donner vie à vos idées.</p>
     </footer>
   )
 }
